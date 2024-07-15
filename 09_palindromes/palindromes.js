a1f561490e5cfa8d6ab499641dfcecbd36d348a1
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-07-16 00:42:40 +0200

    Solved palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversedString = cleanedString.split("").reverse().join("");
+    if (cleanedString === reversedString) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
