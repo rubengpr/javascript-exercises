4bc2b5ceb5dec1cdde82235bf0b5a685e0260a7e
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-07-16 20:21:28 +0200

    Solved getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,13 @@
-const getTheTitles = function() {
-
+function getTheTitles() {
+    const newArray = []
+    books.forEach(function(item) {
+        const itemTitle = item.title
+        newArray.push(itemTitle)
+    })
+    return newArray
 };
+
+getTheTitles();
 
 // Do not edit below this line
 module.exports = getTheTitles;
