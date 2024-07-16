dd834b37c51dd7ffa7e1ebcb67e7e7a0ddb561f6
Ruben Godoy <61808374+rubengpr@users.noreply.github.com>
2024-07-16 19:38:57 +0200

    Solved Fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0) {
+        return "OOPS";
+    }
 
+    const array = [0, 1];
+    for (let i = 2; i <= number; i++) {
+        const newFiboValue = array[i - 1] + array[i - 2];
+        array.push(newFiboValue);
+    }
+
+    return array[number];
 };
+
+console.log(fibonacci(5));  // Outputs: 5
+console.log(fibonacci(-1)); // Outputs: OOPS
+
 
 // Do not edit below this line
 module.exports = fibonacci;
